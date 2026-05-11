5702d7040bc2ee81f3dd4a5e9b5b33ac04c63393
Deploy <deploy@local.app>
2026-05-11 19:26:14 +0300

    setup github pages deploy

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { inspectAttr } from 'kimi-plugin-inspect-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '/club-shou/',
   plugins: [inspectAttr(), react()],
   server: {
     port: 3000,

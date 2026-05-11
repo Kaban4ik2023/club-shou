bb172dafd8ff707fc922ae38ef3be2d30b26fb58
Deploy <deploy@local.app>
2026-05-11 19:29:10 +0300

    fix base url for custom domain

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { inspectAttr } from 'kimi-plugin-inspect-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/club-shou/',
+  base: '/',
   plugins: [inspectAttr(), react()],
   server: {
     port: 3000,
